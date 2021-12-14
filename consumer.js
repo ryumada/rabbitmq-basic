761f106a53525f7caceec4615bd92a3e809bca46
ryumada <rizukiryumada@gmail.com>
2021-12-14 14:16:07 +0700

    created consumer to receive queue from RabbitMQ

---
+++ consumer.js
@@ -0,0 +1,20 @@
+const amqp = require('amqplib');
+
+const init = async () => {
+  const connection = await amqp.connect('amqp://localhost');
+  const channel = await connection.createChannel();
+
+  const queue = 'ryumada';
+
+  // ini untuk memastikan apakah queue dengan nama 'ryumada' telah dibuat
+  await channel.assertQueue(queue, {
+    durable: true,
+  });
+
+  channel.consume(queue, (message) => {
+    console.log(`Menerima pesan dari queue 
+    ${queue}: ${message.content.toString()}`);
+  }, {noAck: true});
+};
+
+init();
